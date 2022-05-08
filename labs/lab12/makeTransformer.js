ff4a357138207ec673f4fb27f414f019495bfef6
Chris Minnick <chris@minnick.com>
2022-05-08 05:53:45 -0700

    fixing lab files

--- labs/lab12/makeTransformer.js
+++ labs/lab12/makeTransformer.js
@@ -1,14 +1,14 @@
 const Transform = require('stream').Transform;
 
-var makeTransformer = (finder, replacer) => {
-	const myTransform = new Transform({ 
-    	transform(chunk, encoding, callback){
-        	chunk = chunk.toString().replace(new RegExp(finder, "gi"), replacer); 
-        	this.push(chunk);
-        	callback();
-    	} 
-	});
-	return myTransform;
-}
+let makeTransformer = (finder, replacer) => {
+  const myTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      chunk = chunk.toString().replace(new RegExp(finder, 'gi'), replacer);
+      this.push(chunk);
+      callback();
+    },
+  });
+  return myTransform;
+};
 
 module.exports = makeTransformer;
